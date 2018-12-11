3b0bc8566a43d61cc459edb425cdfd0c61fd9cbd
燎 戸島 <160275@jc-21.jp>
2018-12-11 12:18:57 +0900

    ？・。・？

---
+++ Xamarin/Xamarin/Xamarin/fixed_cost.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Xamarin
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class fixed_cost : ContentPage
+    {
+        public fixed_cost()
+        {
+            InitializeComponent();
+
+            var komokuList = new List<string>
+        {
+            "食費",
+            "日用品",
+            "交通費",
+            "医療費",
+            "光熱費",
+            "美容衣服",
+            "趣味",
+            "酒タバコ",
+            "その他",
+        };
+
+            foreach (var item in komokuList)
+            {
+                this.komoku.Items.Add(item);
+            }
+
+        }
+
+	}
+}
---
+++ Xamarin/Xamarin/Xamarin/insert.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Xamarin
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class insert : CarouselPage
+	{
+		public insert (List<DateTime> dt)
+		{
+			InitializeComponent ();
+
+            Calendar.Text = dt[0].ToString("yyyy年MM月dd日");
+		}
+
+
+
+    }
+
+}
